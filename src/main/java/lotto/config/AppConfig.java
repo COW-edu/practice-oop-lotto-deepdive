40471ec0cb51ac48a2acad809de6903c11017114
Seooooo24 <ahyeungii@gmail.com>
2025-03-27 04:43:50 +0900

    feat: config 클래스 작성

---
+++ src/main/java/lotto/config/AppConfig.java
@@ -0,0 +1,50 @@
+package lotto.config;
+
+import lotto.controller.LottoController;
+import lotto.controller.LottoControllerImpl;
+import lotto.model.LottoCountRepository;
+import lotto.model.LottoCountRepositoryImpl;
+import lotto.model.LottoNumberRepository;
+import lotto.model.LottoNumberRepositoryImpl;
+import lotto.model.WinningNumberRepository;
+import lotto.model.WinningNumberRepositoryImpl;
+import lotto.service.LottoCountService;
+import lotto.service.LottoCountServiceImpl;
+import lotto.service.LottoNumberService;
+import lotto.service.LottoNumberServiceImpl;
+import lotto.service.WinningNumberService;
+import lotto.service.WinningNumberServiceImpl;
+
+public class AppConfig {
+
+    public LottoController lottoController() {
+        return new LottoControllerImpl(lottoCountService(), lottoNumberService(),
+                winningNumberService());
+    }
+
+    public LottoCountService lottoCountService() {
+        return new LottoCountServiceImpl(lottoCountRepository());
+    }
+
+    public LottoNumberService lottoNumberService() {
+        return new LottoNumberServiceImpl(lottoCountRepository(), lottoNumberRepository());
+    }
+
+    public WinningNumberService winningNumberService() {
+        return new WinningNumberServiceImpl(winningNumberRepository());
+    }
+
+    public LottoCountRepository lottoCountRepository() {
+        return new LottoCountRepositoryImpl();
+    }
+
+
+    public LottoNumberRepository lottoNumberRepository() {
+        return new LottoNumberRepositoryImpl();
+    }
+
+    public WinningNumberRepository winningNumberRepository() {
+        return new WinningNumberRepositoryImpl();
+    }
+
+}
