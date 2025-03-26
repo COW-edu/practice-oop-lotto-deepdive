3ea3861ea5959da066a1b39f018f4def23d74ef4
JinHyeon <tiemo0708@naver.com>
2025-03-27 02:13:47 +0900

    refactor: format 중복호출 제거

--- src/main/java/lotto/adapter/in/console/ConsoleOutputView.java
+++ src/main/java/lotto/adapter/in/console/ConsoleOutputView.java
@@ -9,7 +9,7 @@ public class ConsoleOutputView {
     private static final String PURCHASED_TICKETS_MESSAGE = "\n%d개를 구매했습니다.";
 
     public void printLottoTickets(LottoTickets lootoTickets){
-        System.out.println(String.format(PURCHASED_TICKETS_MESSAGE, lootoTickets.size()));
+        System.out.printf((PURCHASED_TICKETS_MESSAGE) + "%n", lootoTickets.size());
         List<Lotto> tickets = lootoTickets.getTickets();
         for (Lotto ticket : tickets) {
             System.out.println(ticket.getNumbers());
