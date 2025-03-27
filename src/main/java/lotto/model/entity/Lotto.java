8ccabbdc6f235fac466553dcb85689b8002dd928
Seooooo24 <ahyeungii@gmail.com>
2025-03-28 01:24:43 +0900

    feat: 로또 번호 관련 예외 처리 로직 추가

--- src/main/java/lotto/model/entity/Lotto.java
+++ src/main/java/lotto/model/entity/Lotto.java
@@ -1,10 +1,15 @@
 package lotto.model.entity;
 
+import static lotto.constants.ErrorMessages.DUPLICATED_NUMBER_ERROR_MESSAGE;
+import static lotto.constants.ErrorMessages.EXCEED_LOTTO_SIZE_ERROR_MESSAGE;
 import static lotto.constants.LottoConstants.LOTTO_SIZE;
 
 import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Lotto {
+
     private final List<Integer> numbers;
 
     public Lotto(List<Integer> numbers) {
@@ -13,8 +18,20 @@ public class Lotto {
     }
 
     private void validate(List<Integer> numbers) {
+        validateSize(numbers);
+        validateDuplicate(numbers);
+    }
+
+    private static void validateSize(List<Integer> numbers) {
         if (numbers.size() != LOTTO_SIZE) {
-            throw new IllegalArgumentException();
+            throw new IllegalArgumentException(EXCEED_LOTTO_SIZE_ERROR_MESSAGE);
+        }
+    }
+
+    private static void validateDuplicate(List<Integer> numbers) {
+        Set<Integer> uniqueNumbers = numbers.stream().collect(Collectors.toSet());
+        if (uniqueNumbers.size() != numbers.size()) {
+            throw new IllegalArgumentException(DUPLICATED_NUMBER_ERROR_MESSAGE);
         }
     }
 
