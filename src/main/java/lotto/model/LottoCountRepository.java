78b4ece1c239ebf63b612dfa1ce15fe80b1b9733
Seooooo24 <ahyeungii@gmail.com>
2025-03-27 21:05:34 +0900

    feat: LottoCount 관련 repository 구현

--- src/main/java/lotto/model/LottoCountRepository.java
+++ src/main/java/lotto/model/LottoCountRepository.java
@@ -2,7 +2,11 @@ package lotto.model;
 
 public interface LottoCountRepository {
 
+    void saveInputMoney(int money);
+
     void saveLottoCount(int lottoCount);
 
     int getLottoCount();
+
+    int getInputMoney();
 }
